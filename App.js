de0082e9a2bb0f1c427700781723d93394ef66df
iamshimer <ahamadshimer@gmail.com>
2022-01-28 16:08:23 +0530

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onLoginPressed = () => {
+    console.log(email, password);
+    Alert.alert("Login Successful");
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        style={styles.circleImage}
+        source={require("./assets/ironman.jpg")}
+      />
+      <View>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Enter your email"
+          onChangeText={setEmail}
+        />
+        <TextInput
+          style={styles.inputText}
+          placeholder="Enter your passeord"
+          secureTextEntry={true}
+          onChangeText={setPassword}
+        />
+        <Button
+          title="Login"
+          style={styles.submitButton}
+          onPress={onLoginPressed}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +48,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "pink",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  circleImage: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+  },
+  inputText: {
+    width: 300,
+    height: 38,
+    borderWidth: 1,
+    marginBottom: 17,
+    borderRadius: 15,
+    backgroundColor: "white",
+    paddingLeft: 10,
+  },
+  submitButton: {
+    width: 100,
+    height: 50,
+    borderRadius: 50,
   },
 });
